fbdf748de0cdc9b58f2b8edca7537738bf89c17b
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-12-18 22:42:31 +1100

    Slightly better arrows on the figure.

--- figs.py
+++ figs.py
@@ -1,4 +1,5 @@
 from pylab import *
+from matplotlib.patches import FancyArrow
 
 # Foreman-Mackey's taste in figures
 rc("font", size=20, family="serif", serif="Computer Sans")
@@ -13,6 +14,19 @@ ylabel('Poisson Rate')
 ylim([0., 1.1])
 axvline(1., color='r', linestyle='--')
 title('A Word')
+
+# Build an arrow.
+ar1 = FancyArrow(1., exp(-1.), -0.5, 0., length_includes_head=True,
+		color='k', width=0.01)
+ar2 = FancyArrow(1., exp(-1.), 1.5, 0., length_includes_head=True,
+		color='k', width=0.01)
+ax = gca()
+# Add the arrow to the axes.
+ax.add_artist(ar1)
+ax.add_artist(ar2)
+
+show()
+
 savefig('documents/word.pdf', bbox_inches='tight')
 
 

bc2177c3b4e1579e25ebe20bb4f9dcf13cb534db
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-12-18 21:29:18 +1100

    Log prior density

--- burstmodel.py
+++ burstmodel.py
@@ -110,6 +110,15 @@ def log_likelihood(lambdas, data):
 
 
 
-
-
+# Our prior for a SINGLE WORD
+# Input: parameter vector, which gets unpacked into named things
+# feel free to change the order if that's how you defined it - BJB
+def log_prior(params):
+    [amplitude, scale, skew] = unpack(params)
+    if amplitude < np.log(-10.) or amplitude > np.log(10.) or
+        scale < np.log(-6.) or scale > np.log(12.) or skew < np.log(-1.5) or
+        skew > np.log(1.5):
+        return -np.Inf
+    return 0.
+    # okay now do it proper...
 

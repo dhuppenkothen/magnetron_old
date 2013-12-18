e3fecd515d454ebf2e69ed5a000e38245ca3ff0a
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-12-18 23:03:28 +1100

    Arrows and text

--- figs.py
+++ figs.py
@@ -16,17 +16,21 @@ axvline(1., color='r', linestyle='--')
 title('A Word')
 
 # Build an arrow.
-ar1 = FancyArrow(1., exp(-1.), -0.5, 0., length_includes_head=True,
-		color='k', width=0.01)
-ar2 = FancyArrow(1., exp(-1.), 1.5, 0., length_includes_head=True,
-		color='k', width=0.01)
+ar1 = FancyArrow(1., 1.01*exp(-1.), -0.5, 0., length_includes_head=True,
+		color='k', head_width=0.01, head_length=0.2, width=0.001, linewidth=1)
+ar2 = FancyArrow(1., 0.99
+*exp(-1.), 1.5, 0., length_includes_head=True,
+		color='k', head_width=0.01, head_length=0.2, width=0.001, linewidth=1)
 ax = gca()
 # Add the arrow to the axes.
 ax.add_artist(ar1)
 ax.add_artist(ar2)
 
-show()
+# Add text
+text(-0.4, 1.*exp(-1.), r'$\tau$')
+text(2.7, 1.*exp(-1.), r'$S\tau$')
 
 savefig('documents/word.pdf', bbox_inches='tight')
+show()
 
 

2bd0d48bba77334c14d3f547bb064d0b1a2eb073
marcus frean <marcus@ecs.vuw.ac.nz>
2014-01-13 00:30:38 +1300

    plot_raw does very quick crude test for skew: none evident

--- data/plot_raw.py
+++ data/plot_raw.py
@@ -7,12 +7,32 @@ import numpy as np
 import pylab as pl
 import glob, sys, math, optparse
 
+def whichWay(t,y):
+        # is the nearest other maximum to the left or the right of the "global" maximum?
+        maxpoint = np.argmax(y)
+        nextmax,prevmax = maxpoint,maxpoint
+
+        tmpval = y[maxpoint]
+        while (y[nextmax] <= tmpval):
+            tmpval = y[nextmax]
+            nextmax = nextmax+1
+        gap_to_next = nextmax - maxpoint
+
+        tmpval = y[maxpoint]
+        while (y[prevmax] <= tmpval):
+            tmpval = y[prevmax]
+            prevmax = prevmax-1
+        gap_to_prev = maxpoint - prevmax
+        if (gap_to_next < gap_to_prev): return 'right'
+        elif (gap_to_next > gap_to_prev): return 'left'
+        return 'tied'
+
 
 if __name__ == "__main__":
 
     parser = optparse.OptionParser(usage="usage %prog [options]")
-    parser.add_option("-n",type = "int",dest = "numplots", default=9,
-                      help="some number of data files to plot (default is 9)")
+    parser.add_option("-n",type = "int",dest = "numplots", default=99999,
+                      help="some number of data files to plot (default is all)")
     parser.add_option("-i",type = "int",dest = "index",
                       help="index (just in the file listing) of a specific file to plot")
     opts, args = parser.parse_args()
@@ -40,12 +60,25 @@ if __name__ == "__main__":
     for i,name in enumerate(names):
         pl.subplot(numrows,numcols,i+1)
         m = datafiles[name]
-        first = np.min(m[:,0])
-        m[:,0] = m[:,0] - first
-        pl.text(np.min(m[:,0]),np.max(m[:,1]),name, color='red', fontsize=8,alpha=0.3)
-        if len(names)>1: pl.text(np.min(m[:,0]),np.max(m[:,1])/2,str(i), color='blue')
-        pl.plot(m[:,0],m[:,1],'-k')
+        t = m[:,0]
+        y = m[:,1]
+        first = np.min(t)
+        t = t - first
+        
+        pl.text(np.min(t),np.max(y),name, color='red', fontsize=8,alpha=0.3)
+        if len(names)>1: pl.text(np.min(t),np.max(y)/2,str(i), color='k')
+
+        # Just for kicks: find the max, and test whether the nearest
+        # NEXT max is before (show as red) or after (show as blue). This is
+        # a really really really crude test of ANY kind of skew,
+        # either in spike time, or skew-per-spike. Result: I don't see any.
+        nearestMaxDirection = whichWay(t,y)
+        colour = 'black'
+        if nearestMaxDirection == 'right': colour='blue'
+        if nearestMaxDirection == 'left': colour='red'
+        pl.plot(t,y,'-',color=colour)
         pl.axis("off")
+
 
     outfile = 'transients'
     pl.savefig(outfile,dpi=150)

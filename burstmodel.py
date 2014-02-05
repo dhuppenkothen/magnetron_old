5b2a7119f16fec321e00e412611009a52906f769
dhuppenkothen <d.huppenkothen@gmail.com>
2014-02-05 11:44:15 +0100

    commented out all of old code

--- burstmodel.py
+++ burstmodel.py
@@ -712,69 +712,69 @@ if __name__ == '__main__':
 #### OLD IMPLEMENTATION! ##############################################
 #######################################################################
 
-class DictPosterior(object):
+#class DictPosterior(object):
 
-    """
-    note: implicit assumption is that array times is equally spaced
-          nbins_data: number of bins in data
-          nbins: multiplicative factor for model light curve bins
-    """
-    def __init__(self, times, counts, wordmodel, nbins=10):
-        self.times = times
-        self.counts = counts
-        self.model = wordmodel
+#    """
+#    note: implicit assumption is that array times is equally spaced
+#          nbins_data: number of bins in data
+#          nbins: multiplicative factor for model light curve bins
+#    """
+#    def __init__(self, times, counts, wordmodel, nbins=10):
+#        self.times = times
+#        self.counts = counts
+#        self.model = wordmodel
         #self.npar = npar
 
-        self.Delta = times[1]-times[0]
-        self.nbins_data = len(times)
-        self.nbins = nbins
+#        self.Delta = times[1]-times[0]
+#        self.nbins_data = len(times)
+#        self.nbins = nbins
 
 
 
-    def logprior(self, theta):
+#    def logprior(self, theta):
 
         # Our prior for a SINGLE WORD
         # Input: parameter vector, which gets unpacked into named things
         # feel free to change the order if that's how you defined it - BJB
 
-        saturation_countrate = 3.5e5 ### in counts/s
-        T = self.times[-1] - self.times[0]
+#        saturation_countrate = 3.5e5 ### in counts/s
+#        T = self.times[-1] - self.times[0]
     
  
-        skew, bkg, scale, theta_evt = unpack(theta)
+#        skew, bkg, scale, theta_evt = unpack(theta)
 
-        if  scale < self.Delta or scale > T or skew < np.exp(-1.5) or skew > np.exp(3.0) or \
-                bkg < 0 or bkg > saturation_countrate:
-            return -np.Inf
+#        if  scale < self.Delta or scale > T or skew < np.exp(-1.5) or skew > np.exp(3.0) or \
+#                bkg < 0 or bkg > saturation_countrate:
+#            return -np.Inf
 
-        all_event_times = theta_evt[:,0]
-        all_amp = theta_evt[:,1]
-        if np.min(all_event_times) < self.times[0] or np.max(all_event_times) > self.times[-1] or \
-                np.min(all_amp) < 0.1/self.Delta or np.max(all_amp) > saturation_countrate:
-            return -np.Inf
+#        all_event_times = theta_evt[:,0]
+#        all_amp = theta_evt[:,1]
+#        if np.min(all_event_times) < self.times[0] or np.max(all_event_times) > self.times[-1] or \
+#                np.min(all_amp) < 0.1/self.Delta or np.max(all_amp) > saturation_countrate:
+#            return -np.Inf
 
-        return 0.
+#        return 0.
 
 
     ## theta = [scale, skew, move1, amp1, move2, amp2]    
-    def loglike(self, theta):
+#    def loglike(self, theta):
   
 
         ### unpack theta:
-        skew, bkg, scale, theta_evt = unpack(theta)
-        lambdas = model_means(self.Delta, self.nbins_data, skew, bkg, scale, theta_evt, nbins=self.nbins)
+#        skew, bkg, scale, theta_evt = unpack(theta)
+#        lambdas = model_means(self.Delta, self.nbins_data, skew, bkg, scale, theta_evt, nbins=self.nbins)
 
-        return log_likelihood(lambdas, self.counts)
+#        return log_likelihood(lambdas, self.counts)
 
     ## change parameters:
     
 
-    def logposterior(self, theta):
-        return self.logprior(theta) + self.loglike(theta)
+#    def logposterior(self, theta):
+#        return self.logprior(theta) + self.loglike(theta)
 
 
-    def __call__(self, theta):
-        return self.logposterior(theta)
+#    def __call__(self, theta):
+#        return self.logposterior(theta)
 
 
 
@@ -790,104 +790,104 @@ class DictPosterior(object):
 #    return y
 #######
 
-def event_rate(time, event_time, scale, amp, skew):
+#def event_rate(time, event_time, scale, amp, skew):
 
     #x = x - theta[0]
 
-    time = time - event_time
-    counts = amp*word(time, scale, skew)
+#    time = time - event_time
+#    counts = amp*word(time, scale, skew)
 
-    return counts
+#    return counts
 
 ### note: theta_all is a numpy array of n by m, 
 ### where n is the number of peaks, and m is the number of parameters
 ### per peak
-def model_means(Delta, nbins_data, skew, bkg, scale, theta_evt, nbins=10):
+#def model_means(Delta, nbins_data, skew, bkg, scale, theta_evt, nbins=10):
 
-    delta = Delta/nbins
-    nsmall = nbins_data*nbins
-    time_small = np.arange(nsmall)*delta
-    rate_small = np.zeros(nsmall)
+#    delta = Delta/nbins
+#    nsmall = nbins_data*nbins
+#    time_small = np.arange(nsmall)*delta
+#    rate_small = np.zeros(nsmall)
 
 
-    for (event_time, amp) in theta_evt:
+#    for (event_time, amp) in theta_evt:
 
-        rate_temp = event_rate(time_small, event_time, scale, amp, skew)
-        rate_small = rate_small + rate_temp
+#        rate_temp = event_rate(time_small, event_time, scale, amp, skew)
+#        rate_small = rate_small + rate_temp
 
-    nrow = len(rate_small)/nbins
-    rate_map = rate_small.reshape(nrow, nbins)
-    rate_map_sum = np.sum(rate_map, axis=1)*delta
+#    nrow = len(rate_small)/nbins
+#    rate_map = rate_small.reshape(nrow, nbins)
+#    rate_map_sum = np.sum(rate_map, axis=1)*delta
 
-    rate_map_all = rate_map_sum + bkg*Delta
+#    rate_map_all = rate_map_sum + bkg*Delta
 
-    return rate_map_all
+#    return rate_map_all
 
 ## go from numpy array weird shape
-def unpack(theta):
-    """
-    unpacks the numpy array of parameters into a form that model_means can read.
+#def unpack(theta):
+#    """
+#    unpacks the numpy array of parameters into a form that model_means can read.
 
-    returns: skew, bkg, scale, theta_evt
+#    returns: skew, bkg, scale, theta_evt
 
-    theta_evt is an array of npeaks by 2, where npeaks is the number of peaks
-    each row is (event_time, amp)
-    """
-
-
-    skew = np.exp(theta[0])
-    bkg = np.exp(theta[1])
-    scale = np.exp(theta[2])
+#    theta_evt is an array of npeaks by 2, where npeaks is the number of peaks
+#    each row is (event_time, amp)
+#    """
 
 
-    theta = np.array(theta)
-    theta_evt = copy.copy(theta[3:]).reshape((len(theta)-3)/2, 2)
+#    skew = np.exp(theta[0])
+#    bkg = np.exp(theta[1])
+#    scale = np.exp(theta[2])
 
-    for i in range(len(theta_evt)):
-        theta_evt[i][1] = np.exp(theta_evt[i][1])
 
-    return skew, bkg, scale, theta_evt
+#    theta = np.array(theta)
+#    theta_evt = copy.copy(theta[3:]).reshape((len(theta)-3)/2, 2)
+
+#    for i in range(len(theta_evt)):
+#        theta_evt[i][1] = np.exp(theta_evt[i][1])
+
+#    return skew, bkg, scale, theta_evt
 
 
 ## go from weird shape to numpy array
-def pack(skew, bkg, scale, theta_evt):
+#def pack(skew, bkg, scale, theta_evt):
 
-    theta = np.zeros(len(theta_evt.flatten())+3)
-    theta[0] = np.log(skew)
-    theta[1] = np.log(bkg)
-    theta[2] = np.log(scale)
+#    theta = np.zeros(len(theta_evt.flatten())+3)
+#    theta[0] = np.log(skew)
+#    theta[1] = np.log(bkg)
+#    theta[2] = np.log(scale)
 
-    for i in range(len(theta_evt)):
-        theta_evt[i][1] = np.log(theta_evt[i][1])
+#    for i in range(len(theta_evt)):
+#        theta_evt[i][1] = np.log(theta_evt[i][1])
 
-    theta[3:] = theta_evt.flatten()
+#    theta[3:] = theta_evt.flatten()
 
-    return theta
+#    return theta
 
 
 # noinspection PyNoneFunctionAssignment
-def model_burst():
+#def model_burst():
 
-    times = np.arange(1000.0)/10000.0
-    counts = np.random.poisson(2000.0, size=len(times))
+#    times = np.arange(1000.0)/10000.0
+#    counts = np.random.poisson(2000.0, size=len(times))
 
-    skew = 3.0
-    scale = 0.005
-    bkg = 5.0
+#    skew = 3.0
+#    scale = 0.005
+#    bkg = 5.0
 
-    nspikes = 2
-    theta_evt = np.zeros((nspikes,2))
+#    nspikes = 2
+#    theta_evt = np.zeros((nspikes,2))
 
-    for i in range(nspikes):
-        theta_evt[i] = [np.random.rand()*times[-1], 10.0/(times[1]-times[0])]
-        
-    theta = pack(skew, bkg, scale, theta_evt)
+#    for i in range(nspikes):
+#        theta_evt[i] = [np.random.rand()*times[-1], 10.0/(times[1]-times[0])]
 
-    return theta
+#    theta = pack(skew, bkg, scale, theta_evt)
 
-def initial_guess(times, counts, skew, bkg, scale, theta_evt):
+#    return theta
 
-    times = times - times[0]
+#def initial_guess(times, counts, skew, bkg, scale, theta_evt):
+
+#    times = times - times[0]
 
     ### initialise guess for burst 090122218, tstart = 47.4096 
     #skew = 5.0
@@ -896,112 +896,112 @@ def initial_guess(times, counts, skew, bkg, scale, theta_evt):
     #theta_evt = np.array([[0.82, 30000], [0.87, 60000], [0.95, 50000], [1.05, 20000]])
 
 
-    Delta = times[1]-times[0]
-    nbins_data = len(times)
+#    Delta = times[1]-times[0]
+#    nbins_data = len(times)
 
-    counts_model = model_means(Delta, nbins_data, skew, bkg, scale, theta_evt, nbins=10)
+#    counts_model = model_means(Delta, nbins_data, skew, bkg, scale, theta_evt, nbins=10)
 
-    figure()
-    plt.plot(times, counts, 'k')
-    plt.plot(times, counts_model, 'r')
-    plt.xlabel('Time [s]', fontsize=18)
-    plt.ylabel('Counts per bin', fontsize=18)
-    plt.title('Light curve with initial guess for model', fontsize=18)
+#    figure()
+#    plt.plot(times, counts, 'k')
+#    plt.plot(times, counts_model, 'r')
+#    plt.xlabel('Time [s]', fontsize=18)
+#    plt.ylabel('Counts per bin', fontsize=18)
+#    plt.title('Light curve with initial guess for model', fontsize=18)
 
-    theta = pack(skew, bkg, scale, np.array(theta_evt))
+#    theta = pack(skew, bkg, scale, np.array(theta_evt))
 
-    return theta, counts_model
+#    return theta, counts_model
 
 
 ### put in burst times array and counts array
-def test_burst(times, counts, theta_guess, namestr = 'testburst', nwalker=32):
+#def test_burst(times, counts, theta_guess, namestr = 'testburst', nwalker=32):
 
-    skew, bkg, scale, theta_evt = unpack(theta_guess)
+#    skew, bkg, scale, theta_evt = unpack(theta_guess)
 
     #Delta = times[1]-times[0]
     #nbins_data = len(times)
  
-    theta, counts_model = initial_guess(times, counts, skew, bkg, scale, theta_evt)
+#    theta, counts_model = initial_guess(times, counts, skew, bkg, scale, theta_evt)
 
-    figure()
-    plt.plot(times, counts, 'k')
-    plt.plot(times, counts_model, 'r')
-    plt.xlabel('Time [s]', fontsize=18)
-    plt.ylabel('Counts per bin', fontsize=18)
-    plt.title('Light curve with initial guess for model', fontsize=18)
-    plt.savefig(namestr + '_initialguess.png', format='png')
-    plt.close()
+#    figure()
+#    plt.plot(times, counts, 'k')
+#    plt.plot(times, counts_model, 'r')
+#    plt.xlabel('Time [s]', fontsize=18)
+#    plt.ylabel('Counts per bin', fontsize=18)
+#    plt.title('Light curve with initial guess for model', fontsize=18)
+#    plt.savefig(namestr + '_initialguess.png', format='png')
+#    plt.close()
 
-    theta = pack(skew, bkg, scale, theta_evt)   
+#    theta = pack(skew, bkg, scale, theta_evt)
 
-    lpost = DictPosterior(times, counts)
+#    lpost = DictPosterior(times, counts)
    
-    if nwalker < 2*len(theta):
-        nwalker = 2*len(theta)
+#    if nwalker < 2*len(theta):
+#        nwalker = 2*len(theta)
 
-    p0 = [theta+np.random.rand(len(theta))*1.0e-3 for t in range(nwalker)]
+#    p0 = [theta+np.random.rand(len(theta))*1.0e-3 for t in range(nwalker)]
 
-    sampler = emcee.EnsembleSampler(nwalker, len(theta), lpost)
-    pos, prob, state = sampler.run_mcmc(p0, 200)
-    sampler.reset()
-    sampler.run_mcmc(pos, 1000, rstate0 = state)
+#    sampler = emcee.EnsembleSampler(nwalker, len(theta), lpost)
+#    pos, prob, state = sampler.run_mcmc(p0, 200)
+#    sampler.reset()
+#    sampler.run_mcmc(pos, 1000, rstate0 = state)
 
-    plot_test(times, counts, sampler.flatchain[-10:])
-    plt.xlabel('Time [s]', fontsize=18)
-    plt.ylabel('Counts per bin', fontsize=18)
-    plt.title('Light curve with draws from posterior sample', fontsize=18)
-    plt.savefig(namestr + '_posteriorsample.png', format='png')
-    plt.close()
+#    plot_test(times, counts, sampler.flatchain[-10:])
+#    plt.xlabel('Time [s]', fontsize=18)
+#    plt.ylabel('Counts per bin', fontsize=18)
+#    plt.title('Light curve with draws from posterior sample', fontsize=18)
+#    plt.savefig(namestr + '_posteriorsample.png', format='png')
+#    plt.close()
 
 
     ### quick hack to save emcee sampler to disc
-    f = open(namestr + '_sampler.dat', 'w')
-    pickle.dump(sampler, f)
-    f.close()
+#    f = open(namestr + '_sampler.dat', 'w')
+#    pickle.dump(sampler, f)
+#    f.close()
 
-    return 
-
-
-def plot_test(times, counts, theta):
-
-    plt.plot(times, counts, 'k')
+#    return
 
 
-    Delta = times[1]-times[0]
-    nbins_data = len(times)
+#def plot_test(times, counts, theta):
+
+#    plt.plot(times, counts, 'k')
 
 
-    for t in np.atleast_2d(theta):
+#    Delta = times[1]-times[0]
+#    nbins_data = len(times)
 
-        skew, bkg, scale, theta_evt = unpack(t)
-        counts_model = model_means(Delta, nbins_data, skew, bkg, scale, theta_evt, nbins=10)
-        plt.plot(times, counts_model, 'r')
 
-    return
+#    for t in np.atleast_2d(theta):
+
+#        skew, bkg, scale, theta_evt = unpack(t)
+#        counts_model = model_means(Delta, nbins_data, skew, bkg, scale, theta_evt, nbins=10)
+#        plt.plot(times, counts_model, 'r')
+
+#    return
 
 # Poisson log likelihood based on a set of rates
 # log[ prod exp(-lamb)*lamb^x/x! ]
 # exp(-lamb)
 
 ### lambdas: numpy array of Poisson rates: mean expected integrated in a bin
-import scipy.special
-def log_likelihood(lambdas, data):
+#import scipy.special
+#def log_likelihood(lambdas, data):
 
-    return -np.sum(lambdas) + np.sum(data*np.log(lambdas))\
-        -np.sum(scipy.special.gammaln(data + 1))
+#    return -np.sum(lambdas) + np.sum(data*np.log(lambdas))\
+#        -np.sum(scipy.special.gammaln(data + 1))
 
 
-def conversion(filename):
-    f=open(filename, 'r')
-    output_lists=defaultdict(list)
-    for line in f:
-        if not line.startswith('#'):
-             line=[value for value in line.split()]
-             for col, data in enumerate(line):
-                 output_lists[col].append(data)
-    return output_lists
+#def conversion(filename):
+#    f=open(filename, 'r')
+#    output_lists=defaultdict(list)
+#    for line in f:
+#        if not line.startswith('#'):
+#             line=[value for value in line.split()]
+#             for col, data in enumerate(line):
+#                 output_lists[col].append(data)
+#    return output_lists
 
-#def main():
+##def main():
 
 #    data = conversion(filename)
 #    times = np.array([float(t) for t in data[0]])
